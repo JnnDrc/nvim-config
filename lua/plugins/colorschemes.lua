01332070c87dbfd83c3fbd8b76581faf8cc0baa0
JnnDrc <93061677+JnnDrc@users.noreply.github.com>
2024-11-24 00:38:11 -0300

    'lala'

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,23 +1,24 @@
 return {
   -- set the colorscheme in init.lua
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
-    "morhetz/gruvbox",
-    name = "gruvbox",
-  },
-  {
-    "loctvl842/monokai-pro.nvim",
-    name = "monokai-pro"
-  },
-  {
-    "shaunsingh/nord.nvim",
-    name = "nord"
-  },
-  "doums/darcula",
-  name = "darcula"
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+    },
+    {
+        "morhetz/gruvbox",
+    },
+    {
+        "loctvl842/monokai-pro.nvim",
+    },
+    {
+        -- "shaunsingh/nord.nvim",
+    },
+    {
+        "doums/darcula",
+    },
+    {
+        "navarasu/onedark.nvim",
+    }
 }
